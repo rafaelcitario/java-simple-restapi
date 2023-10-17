372aa4be09b7011f657d70079d1d9f0b5177c6e9
rafaelcitario <contato.rafaelgomes@outlook.com>
2023-10-17 19:55:37 -0300

    configuração do projeto

---
+++ src/main/java/com/rafaelcitario/javasimplerestapi/JavaSimpleRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelcitario.javasimplerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSimpleRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSimpleRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelcitario/javasimplerestapi/JavaSimpleRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelcitario.javasimplerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSimpleRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
